3947d5088f1d863452034858af40e863d07dd18f
Simiraider <simon.flomenboim@gmail.com>
2025-08-06 08:54:43 -0300

    escena principal

---
+++ Assets/Scripts/CargarEscPrincipal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CargarEscPrincipal : MonoBehaviour
+{
+    public void CargarPrincipal()
+    {
+        SceneManager.LoadScene("Escena Principal");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
