d704903ef01b9cdbca982e04a56da42cf6eb380d
Bautista2049 <49379221@est.ort.edu.ar>
2025-08-07 10:19:07 -0300

    movimiento funcional

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
+    public float velocidad = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      float horizontal = Input.GetAxis("Horizontal"); 
+      float vertical = Input.GetAxis("Vertical");     
+      Vector3 direccion = new Vector3(horizontal, 0f, vertical);
+      transform.Translate(direccion * velocidad * Time.deltaTime);
     }
 }
