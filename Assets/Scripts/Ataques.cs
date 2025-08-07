bb2ed46fde33a0a0ce2353f5337179038ebff927
Simiraider <simon.flomenboim@gmail.com>
2025-08-07 10:26:13 -0300

    funcionalidad de ataques

---
+++ Assets/Scripts/Ataques.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ataques : MonoBehaviour
+{
+    public void CargarCombate()
+    {
+        SceneManager.LoadScene("Luchar Escena");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
