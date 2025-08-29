bbb9cfcb7f74417493972f3ad6624a17810c9959
Bautista <49379221@est.ort.edu.ar>
2025-08-29 11:56:07 -0300

    Con salsa y picante

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -15,18 +15,22 @@ public class Movimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      float horizontal = Input.GetAxis("Horizontal"); 
-      float vertical = Input.GetAxis("Vertical");     
-      Vector3 direccion = new Vector3(horizontal, 0f, vertical);
-      transform.Translate(direccion * velocidad * Time.deltaTime);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direccion = new Vector3(horizontal, 0f, vertical);
+        transform.Translate(direccion * velocidad * Time.deltaTime);
 
+        // correr con shift
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            velocidad = 7.5f;
+            velocidad = 10f;
         }
+
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             velocidad = 5f;
         }
+
+
     }
 }
