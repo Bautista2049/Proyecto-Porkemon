00b8b17993e72f8665845d5cc8d8cd1b66529ac1
Bautista <49379221@est.ort.edu.ar>
2025-09-19 12:11:20 -0300

    pancho

---
+++ Assets/Scripts/Escena principal/Porkemones/Transicion_Combate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transicion_Combate : MonoBehaviour
+{
+    [SerializeField] private string nombreEscena = "Escena de Combate" ; // pone aca el nombre de la escena a la que queres ir
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) // tu personaje tiene que tener el tag "Player"
+        {
+            SceneManager.LoadScene(nombreEscena);
+            Debug.Log("aaaaaaaa");
+        }
+    }
+}
