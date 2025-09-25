505f8eb3e759fc17077a4a4513acd9265f416876
Simon Flomenboim <simon.flomenboim@gmail.com>
2025-09-25 18:55:26 -0300

    billboard para ui

---
+++ Assets/Billboard.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+            if (mainCamera == null) return;
+        }
+
+        // Este truco hace que el objeto rote para mirar siempre hacia la cámara
+        transform.LookAt(
+            transform.position + mainCamera.transform.rotation * Vector3.forward,
+            mainCamera.transform.rotation * Vector3.up
+        );
+    }
+}
