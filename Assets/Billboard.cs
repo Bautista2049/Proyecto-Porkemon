a0f00c30c169bbb380d3d16cabec874629b2fdb2
Simiraider <simon.flomenboim@gmail.com>
2025-09-29 19:13:01 -0300

    texto world space

--- Assets/Billboard.cs
+++ Assets/Billboard.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
@@ -17,10 +19,21 @@ public class Billboard : MonoBehaviour
             if (mainCamera == null) return;
         }
 
-        // Este truco hace que el objeto rote para mirar siempre hacia la cámara
-        transform.LookAt(
-            transform.position + mainCamera.transform.rotation * Vector3.forward,
-            mainCamera.transform.rotation * Vector3.up
-        );
+        // hacer que el objeto rote para mirar a la cámara sin cambiar su posición (forma simple)
+        Vector3 lookPos = mainCamera.transform.position - transform.position;
+        lookPos.y = 0;
+        if (lookPos.sqrMagnitude > 0.001f)
+        {
+            // Usar LookAt para rotar sin cambiar posición
+            transform.LookAt(transform.position + lookPos);
+
+            // Corregir si el texto está volteado
+            Vector3 euler = transform.eulerAngles;
+            if (euler.y > -1000 && euler.y < 360)
+            {
+                euler.y += 180;
+                transform.eulerAngles = euler;
+            }
+        }
     }
 }
