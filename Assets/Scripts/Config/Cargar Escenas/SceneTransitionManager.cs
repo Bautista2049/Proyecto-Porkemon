9452fbe3687e7f98a9e2abbfc8744bf904511813
Simiraider <simon.flomenboim@gmail.com>
2025-10-06 09:57:32 -0300

    cambio de esc a q

--- Assets/Scripts/Config/Cargar Escenas/SceneTransitionManager.cs
+++ Assets/Scripts/Config/Cargar Escenas/SceneTransitionManager.cs
@@ -35,7 +35,7 @@ public class SceneTransitionManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             LoadScene("Interfaz de Menu");
             Cursor.lockState = CursorLockMode.None;
