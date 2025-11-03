8b59d234234898dd3256e238b86c3fd3d64b5cbb
Simiraider <simon.flomenboim@gmail.com>
2025-11-03 10:09:01 -0300

    aieka

---
+++ Assets/Scripts/Escena principal/Porkemones/Transicion_Jugador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Transicion_Jugador : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nombreEscena);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
