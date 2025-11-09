0e970bfb22f1f0b12ba34482eee1625c3ff2daf3
Simon Flomenboim <simon.flomenboim@gmail.com>
2025-11-09 16:34:53 -0300

    Revert "animator for player"
    This reverts commit 0d5d460e616b57d1fea509ba08e2d7ba528afc2b.

--- Assets/Scripts/Escena Combate/Func/FuncInterno/CharacterAnimation.cs
+++ Assets/Scripts/Escena Combate/Func/FuncInterno/CharacterAnimation.cs
@@ -7,17 +7,18 @@ public class CharacterAnimation : MonoBehaviour
     private Animator animator;
     private Rigidbody rb;
 
+    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
-  
+    // Update is called once per frame
     void Update()
     {
-        float speed = rb.velocity.magnitude;
+        float currentVelocity = rb.velocity.magnitude;
 
-        animator.SetFloat("Speed", speed);
+        animator.SetFloat("velocity", currentVelocity);
     }
 }
