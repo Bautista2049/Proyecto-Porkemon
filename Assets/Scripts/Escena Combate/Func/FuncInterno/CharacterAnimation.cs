e99636e9dbe01260cbdae40804c3633c3d49f96a
Simon Flomenboim <simon.flomenboim@gmail.com>
2025-11-11 23:38:23 -0300

    solucion a animator pero nacho hizo mal las animaciones de vuelta

--- Assets/Scripts/Escena Combate/Func/FuncInterno/CharacterAnimation.cs
+++ Assets/Scripts/Escena Combate/Func/FuncInterno/CharacterAnimation.cs
@@ -1,24 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharacterAnimation : MonoBehaviour
 {
-    private Animator animator;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 10f;
+    
     private Rigidbody rb;
+    private Animator animator;
+    private Vector3 movement;
 
-    // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        // Obtiene el Animator del hijo
+        animator = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float currentVelocity = rb.velocity.magnitude;
+        // Entrada del teclado
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        // Calcula la dirección de movimiento
+        movement = new Vector3(horizontal, 0f, vertical).normalized;
+        
+        // Actualiza el parámetro del Animator
+        if (animator != null)
+        {
+            // Solo usamos la magnitud del movimiento (0 = quieto, 1 = moviendose)
+            animator.SetFloat("velocity", movement.magnitude);
+        }
+    }
 
-        animator.SetFloat("velocity", currentVelocity);
+    void FixedUpdate()
+    {
+        // Mueve al personaje
+        if (movement.magnitude >= 0.1f)
+        {
+            // Rota hacia la dirección del movimiento
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            float angle = Mathf.LerpAngle(transform.eulerAngles.y, targetAngle, rotationSpeed * Time.fixedDeltaTime);
+            rb.MoveRotation(Quaternion.Euler(0f, angle, 0f));
+            
+            // Mueve al personaje
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
     }
 }
